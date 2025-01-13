fe960a9cd50a54991180f7be8cfd36b0ff3a493a
Odys420 <Odyssmelis@gmail.com>
2025-01-13 17:06:17 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,52 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include "functions.h"
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
 
-// Αρχικοποιεί τα προϊόντα με δεδομένα
-void setup_items(Item items[]) {
-    for (int i = 0; i < NUM_PRODUCTS; i++) {
-        snprintf(items[i].name, sizeof(items[i].name), "Προϊόν %d", i + 1);
-        items[i].cost = (i + 1) * 5;  // Τιμή προϊόντος: 5, 10, 15 κ.λπ.
-        items[i].stock = 10;          // Αρχικό απόθεμα 10 για κάθε προϊόν
-        items[i].order_count = 0;     // Αρχικοί μετρητές παραγγελιών
-        items[i].sold_count = 0;      // Αρχικοί μετρητές πωλήσεων
-    }
-}
+// Δομή προϊόντος
+typedef struct {
+    char name[50];        // Όνομα προϊόντος
+    float cost;           // Κόστος προϊόντος
+    int stock;            // Απόθεμα προϊόντος
+    int order_count;      // Αιτήματα παραγγελίας
+    int sold_count;       // Πωλήσεις
+} Item;
 
-// Επεξεργασία του πελάτη (αποστολή παραγγελιών στο κατάστημα)
-void customer_process(Item items[], int client_id, int pipe_to_shop[2], int pipe_from_shop[2]) {
-    for (int i = 0; i < ORDERS_PER_CLIENT; i++) {
-        int product_id = rand() % NUM_PRODUCTS; // Επιλέγουμε τυχαίο προϊόν
-        write(pipe_to_shop[1], &product_id, sizeof(product_id));  // Στέλνουμε παραγγελία στο κατάστημα
+// Δηλώσεις συναρτήσεων
+void setup_items(Item items[]);           // Ρύθμιση δεδομένων προϊόντων
+void customer_process(Item items[], int client_id, int pipe_to_shop[2], int pipe_from_shop[2]);  // Επεξεργασία πελάτη
+void shop_process(Item items[], int client_id, int pipe_to_shop[2], int pipe_from_shop[2]);  // Επεξεργασία καταστήματος
+void print_report(Item items[]);          // Εκτύπωση αναφοράς για τα προϊόντα
 
-        int response;
-        read(pipe_from_shop[0], &response, sizeof(response));  // Διαβάζουμε την απάντηση από το κατάστημα
+#endif
 
-        if (response == 1) {
-            printf("Πελάτης %d: Παραγγελία %d για το Προϊόν %d ολοκληρώθηκε\n", client_id + 1, i + 1, product_id + 1);
-        } else {
-            printf("Πελάτης %d: Παραγγελία %d για το Προϊόν %d απέτυχε\n", client_id + 1, i + 1, product_id + 1);
-        }
-    }
-}
-
-// Επεξεργασία του καταστήματος (λήψη παραγγελιών από πελάτες)
-void shop_process(Item items[], int client_id, int pipe_to_shop[2], int pipe_from_shop[2]) {
-    for (int i = 0; i < ORDERS_PER_CLIENT; i++) {
-        int product_id;
-        read(pipe_to_shop[0], &product_id, sizeof(product_id));  // Λήψη παραγγελίας από πελάτη
-        items[product_id].order_count++;  // Αύξηση αιτημάτων για το προϊόν
-
-        int response;
-        if (items[product_id].stock > 0) {
-            items[product_id].stock--;  // Μείωση αποθέματος
-            items[product_id].sold_count++;  // Αύξηση πωλήσεων
-            response = 1;  // Επιτυχής παραγγελία
-        } else {
-            response = 0;  // Αποτυχημένη παραγγελία (εξάντληση αποθέματος)
-        }
-
-        write(pipe_from_shop[1], &response, sizeof(response));  // Αποστολή απάντησης στον πελάτη
-    }
-}
